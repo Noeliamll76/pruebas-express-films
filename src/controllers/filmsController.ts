12cfb0432c41aecdb8c4af31582ca858773d4db7
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-10-19 12:05:11 +0200

    create: delete and update films routes

--- src/controllers/filmsController.ts
+++ src/controllers/filmsController.ts
@@ -9,10 +9,14 @@ const createFilms = (req:Request, res:Response) => {
     //logica para crear peliculas
     return res.send('CREATE FILMS')
 }
+const updateFilmById = (req:Request, res:Response) => {
+    //logica para modificar peliculas
+    return res.send('UPDATE BY ID FILMS')
+}
+const deleteFilmById = (req:Request, res:Response) => {
+    //logica para borrar peliculas
+    return res.send('DELETE BY ID FILMS')
+}
 
 
-
-
-
-
-export{getFilms, createFilms}
+export{getFilms, createFilms, updateFilmById, deleteFilmById}
--- src/routes/filmsRoutes.ts
+++ src/routes/filmsRoutes.ts
@@ -1,22 +1,11 @@
 import { Router } from "express";
-import {createFilms, getFilms} from "../controllers/filmsController"
+import {createFilms, deleteFilmById, getFilms, updateFilmById} from "../controllers/filmsController"
 
 const router = Router()
+
 router.get('/', getFilms)
-
-
 router.post('/', createFilms)
-
-router.put('/:id', (req,res)=>{
-    //logica para actualizar peliculas
-    const filmId=req.params.id
-    return res.send('UPDATE FILMS '+filmId)
-})
-
-router.delete('/:id', (req,res)=>{
-    //logica para borrar peliculas
-    const filmId=req.params.id
-    return res.send('DELETE FILMS '+filmId)
-})
+router.put('/:id', updateFilmById)
+router.delete('/:id', deleteFilmById)
 
 export { router }
