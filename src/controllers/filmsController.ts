6c43441bd495e8275794178a57314b3bd6dafa85
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-10-20 18:31:54 +0200

    feat:bug in deleteFilmById

--- src/controllers/filmsController.ts
+++ src/controllers/filmsController.ts
@@ -1,42 +1,63 @@
 import { Request, Response } from "express"
+import { Film } from "../models/Film"
 
 
-const getFilms = async(req:Request, res:Response) => {
-   const films = await Film.find()
-// logica de lo que yo quiero hacer/devolver
-    return res.send(films)
+const getFilms = async (req: Request, res: Response) => {
+    try {
+        const films = await Film.find()
+        // logica de lo que yo quiero hacer/devolver
+        return res.send(films)
+    } catch (error) { return res.send(error) }
 }
 
-const createFilms = (req:Request, res:Response) => {
+const createFilms = async (req: Request, res: Response) => {
     //logica para crear peliculas
-    console.log('create')
-    console.log(req.body.title)
-    return res.send('CREATE FILMS')
+    try {
+        const newFilm = await Film.create(
+            {
+                title: req.body.title,
+                director: req.body.director
+            }
+        ).save()
+        return res.send(newFilm)
+    } catch (error) { return res.send(error) }
 }
 
-const updateFilmById = (req:Request, res:Response) => {
+const updateFilmById = (req: Request, res: Response) => {
     //logica para modificar peliculas
-// recuperar la id 
-// recuperamos la informacion que queremos actualizar
-// validamos la informacion
-// comprobamos que existe y que es mia
-// guardamos la nueva info
-// devolvemos la respuesta
+    // recuperar la id 
+    // recuperamos la informacion que queremos actualizar
+    // validamos la informacion
+    // comprobamos que existe y que es mia
+    // guardamos la nueva info
+    // devolvemos la respuesta
 
     return res.send('UPDATE BY ID FILMS')
 }
-const deleteFilmById = (req:Request, res:Response) => {
-    //logica para borrar peliculas
-// 1 recuperar la id por req.params o por body
-// 2 comprobar que existe el id y que es mio
-// 3 Si existe la borraremos
 
+const deleteFilmById = async (req: Request, res: Response) => {
+    try {
+        const filmIdToDelete = req.params.id
 
-    return res.send('DELETE BY ID FILMS')
-}
-const getFilmsById = (req:Request, res:Response) => {
-    // logica para recuperar pelicula por id
-        return res.send('GET by id FILMS')
+        const filmDeleted = await Film.delete(
+            {
+                id: parseInt(filmIdToDelete)
+            }
+        )
+        if (filmDeleted.affected) {
+            return res.send("Se ha eliminado correctamente el id " + filmIdToDelete)
+        }
+        return res.send("No se ha podido eliminar el id " + filmIdToDelete)
+            
+    } catch (error) {
+        return res.send(error)
     }
+}
 
-export{getFilms, createFilms, updateFilmById, deleteFilmById, getFilmsById}
+
+const getFilmsById = (req: Request, res: Response) => {
+    // logica para recuperar pelicula por id
+    return res.send('GET by id FILMS')
+}
+
+export { getFilms, createFilms, updateFilmById, deleteFilmById, getFilmsById }
