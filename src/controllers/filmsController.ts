743cd722dd3c2aefbee44dc3f4a582ad670eb4c7
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-10-20 10:15:07 +0200

    feat: connecton to database with typeORM

--- src/controllers/filmsController.ts
+++ src/controllers/filmsController.ts
@@ -5,20 +5,32 @@ const getFilms = (req:Request, res:Response) => {
 // logica de lo que yo quiero hacer/devolver
     return res.send('GET FILMS')
 }
+
 const createFilms = (req:Request, res:Response) => {
     //logica para crear peliculas
     console.log('create')
-
     console.log(req.body.title)
-
     return res.send('CREATE FILMS')
 }
+
 const updateFilmById = (req:Request, res:Response) => {
     //logica para modificar peliculas
+// recuperar la id 
+// recuperamos la informacion que queremos actualizar
+// validamos la informacion
+// comprobamos que existe y que es mia
+// guardamos la nueva info
+// devolvemos la respuesta
+
     return res.send('UPDATE BY ID FILMS')
 }
 const deleteFilmById = (req:Request, res:Response) => {
     //logica para borrar peliculas
+// 1 recuperar la id por req.params o por body
+// 2 comprobar que existe el id y que es mio
+// 3 Si existe la borraremos
+
+
     return res.send('DELETE BY ID FILMS')
 }
 const getFilmsById = (req:Request, res:Response) => {
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import express from "express";
 import { router as routerFilms } from "./routes/filmsRoutes"
-
+import { AppDataSource } from "./db";
 
 
 const app = express()
-
 const PORT = process.env.PORT || 4000
+
 
 // middleware
 app.use(express.json())
 
 app.use('/films', routerFilms)
 
+AppDataSource.initialize()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running ${PORT}`)
+        })
+        console.log('Database connected');
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
 
-app.listen(PORT,()=>{
-    console.log (`Server running ${PORT}`)
-})
