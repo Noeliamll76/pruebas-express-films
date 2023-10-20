07ff8842ade81b52aef23c207ea0e5904a98bb7c
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-10-20 12:05:39 +0200

    feat: films table migration

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import { Film1697789896922 } from "./migration/1697789896922-film"
 export const AppDataSource = new DataSource({
  type: "mysql",
  host: "localhost",
@@ -8,6 +9,7 @@ export const AppDataSource = new DataSource({
  password: "1234",
  database: "fsd-typeorm",
  entities: [],
+ migrations: [Film1697789896922],
  synchronize: false,
  logging: false,
 })
